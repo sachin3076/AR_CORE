64f982c52493a10935f7563c9cbad7a6656f3f62
bharatoyedesi <bharat@oyedesi.co>
2022-10-12 16:28:38 +0530

    second commit

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,5 +1,6 @@
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
-
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 
 
@@ -11,6 +12,12 @@ class My_Home extends StatefulWidget {
 }
 
 class _My_HomeState extends State<My_Home> {
+
+
+  ArCoreController? arCoreController;
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,4 +30,63 @@ class _My_HomeState extends State<My_Home> {
       ),
     );
   }
+  void _onArCoreViewCreated(ArCoreController controller) {
+    arCoreController = controller;
+
+    _addSphere(arCoreController!);
+    _addCylindre(arCoreController!);
+    _addCube(arCoreController!);
+  }
+
+  void _addSphere(ArCoreController controller) {
+    final material = ArCoreMaterial(
+        color: Color.fromARGB(120, 66, 134, 244));
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(0, 0, -1.5),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addCylindre(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Colors.red,
+      reflectance: 1.0,
+    );
+    final cylindre = ArCoreCylinder(
+      materials: [material],
+      radius: 0.5,
+      height: 0.3,
+    );
+    final node = ArCoreNode(
+      shape: cylindre,
+      position: vector.Vector3(0.0, -0.5, -2.0),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addCube(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Color.fromARGB(120, 66, 134, 244),
+      metallic: 1.0,
+    );
+    final cube = ArCoreCube(
+      materials: [material],
+      size: vector.Vector3(0.5, 0.5, 0.5),
+    );
+    final node = ArCoreNode(
+      shape: cube,
+      position: vector.Vector3(-0.5, 0.5, -3.5),
+    );
+    controller.addArCoreNode(node);
+  }
+  void dispose() {
+    arCoreController?.dispose();
+    super.dispose();
+  }
+
 }
